17cb339cc943c510b905c0f33dbd196737fc5d83
somnath <somnathb@nmodulo.com>
2022-08-22 22:44:02 +0530

    Initial commit

---
+++ test/SimpleEmployment.ts
@@ -0,0 +1,32 @@
+import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("SimpleEmployment", function () {
+  
+
+  describe("Onboarding", function() {
+    it("should allow")
+  })
+});
+
+/**
+ * 
+ * Should return the contract data hash correctly
+ * Should return the contract signer correctly given a signature and signing data
+ * 
+ * should allow employer to send signature and signing date
+ * should not allow any other account to send employer signature
+ * should allow retracting offer
+ * should set the start variable after starting
+ * should not allow accounts outside approvers to access this
+ * 
+ * should allow employee to send signature and signing date
+ * should not allow other accounts to send employee sign
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
