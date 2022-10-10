7ffb71b9bd462386d26c28b530c3c8f7f6811d0f
Somnath B <somnathb@nmodulo.com>
2022-10-10 16:56:59 +0530

    Add timelock and withdraw

--- contracts/WordPact.sol
+++ contracts/WordPact.sol
@@ -7,13 +7,13 @@ import "./Structs.sol";
 // import "@openzeppelin/contracts/access/Ownable.sol";
 // https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
 
-enum BeneficiaryType{
+enum BeneficiaryType {
     NONE,
     YES,
     NO
 }
 
-struct Participant{
+struct Participant {
     address addr;
     bool canVote;
     BeneficiaryType beneficiaryType;
@@ -21,11 +21,12 @@ struct Participant{
 
 struct PactData {
     bool isEditable;
+    bool votingEnabled;    
     uint64 maturityTimeStamp;
-    string pactText;
-    uint totalValue;
-    bool votingEnabled;
+    address creator;
+    uint256 totalValue;
     Participant[] participants;
+    string pactText;
 }
 
 contract WordPact {
@@ -34,6 +35,7 @@ contract WordPact {
     uint64 public pactsCounter;
 
     mapping(bytes32 => mapping(address => uint256)) public contributions;
+    mapping(bytes32 => mapping(address => bool)) canWithdraw;
     event logContribution(bytes32 uid, address payer, uint256 amount);
     event logPactCreated(address creator, bytes32 uid);
 
@@ -50,36 +52,49 @@ contract WordPact {
     function createPact(
         bool isEditable_,
         string calldata pactText_,
-        uint256 maturityTimeStamp_,
+        uint256 secondsToMaturity_,
         bool votingEnabled_,
         address[] calldata particpantAddresses_,
         bool[] calldata participantsCanVoteArray_,
         BeneficiaryType[] calldata beneficiaryTypes_
     ) external payable returns (bytes32 uid) {
-        uint participantCount = particpantAddresses_.length;
+        uint256 participantCount = particpantAddresses_.length;
         require(
-          participantsCanVoteArray_.length == participantCount &&
-          beneficiaryTypes_.length == participantCount
+            participantsCanVoteArray_.length == participantCount &&
+                beneficiaryTypes_.length == participantCount
         );
 
         uid = calcUid();
         pacts[uid].isEditable = isEditable_;
+
+
         pacts[uid].pactText = pactText_;
-        pacts[uid].totalValue = msg.value;
-        pacts[uid].maturityTimeStamp = uint64(maturityTimeStamp_);
         pacts[uid].votingEnabled = votingEnabled_;
-        
-        for(uint i=0; i< participantCount; i++){
-          pacts[uid].participants.push(
-            Participant(
-            particpantAddresses_[i],
-            participantsCanVoteArray_[i],
-            beneficiaryTypes_[i]
-          ));
+        pacts[uid].totalValue = msg.value;
+        if (secondsToMaturity_ > 0) {
+            pacts[uid].maturityTimeStamp = uint64(
+                secondsToMaturity_ + block.timestamp
+            );
+        }
+        pacts[uid].creator = msg.sender;
+        canWithdraw[uid][msg.sender] = true;
+
+        for (uint256 i = 0; i < participantCount; i++) {
+            pacts[uid].participants.push(
+                Participant(
+                    particpantAddresses_[i],
+                    participantsCanVoteArray_[i],
+                    beneficiaryTypes_[i]
+                )
+            );
         }
 
         pactsCounter++;
         emit logPactCreated(msg.sender, uid);
+        if (msg.value > 0) {
+            contributions[uid][msg.sender] += msg.value;
+            emit logContribution(uid, msg.sender, msg.value);
+        }
         return uid;
     }
 
@@ -92,6 +107,16 @@ contract WordPact {
         emit logContribution(pactid, msg.sender, msg.value);
     }
 
+    function withdraw(bytes32 pactid, uint amount) external {
+        require(block.timestamp > pacts[pactid].maturityTimeStamp, "Time locked");
+        require(canWithdraw[pactid][msg.sender]);
+        uint amountToSend = pacts[pactid].totalValue;
+        if(amount > 0 && amount <= amountToSend){
+            amountToSend = amount;
+        }
+        pacts[pactid].totalValue -= amountToSend;
+        payable(msg.sender).transfer(amountToSend);
+    } 
     //Getters
     function getPact(bytes32 pactid)
         external
@@ -99,12 +124,6 @@ contract WordPact {
         returns (PactData memory pactData)
     {
         return pacts[pactid];
-    }
-
-    //Setters
-
-    function badSetEditable(bytes32 pactid, bool isEditable_) public {
-        pacts[pactid].isEditable = isEditable_;
     }
 
     function setText(bytes32 pactid, string memory pactText_) public {
--- test/WordPact.ts
+++ test/WordPact.ts
@@ -14,15 +14,22 @@ async function setSigners() {
     [creator, employee, employerDelegate, employeeDelegate, thirdParty, arbitrator1, arbitrator2] = await ethers.getSigners()
 }
 
-async function deployToDisputePact(suggestedAmt: BigNumberish) {
-    // let pact = await deployAndSignRandomPact()
-    // await pact.delegate([employerDelegate.address], true)
-    // await pact.connect(employee).delegate([employeeDelegate.address], true)
-    // await pact.connect(employerDelegate).start()
-    // await pact.connect(employerDelegate).terminate()
-    // await pact.connect(employerDelegate).fNf({ value: suggestedAmt })
-    // await pact.connect(employeeDelegate).dispute(suggestedAmt)
-    // return pact
+async function createNewPact(
+    value = 0,
+    isEditable_ = false,
+    pactText_ = "Test pact",
+    secondsToMaturity_ = 0,
+    votingEnabled_ = true,
+    particpantAddresses_ = ["0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"],
+    participantsCanVoteArray_ = [false],
+    beneficiaryTypes = [0],
+) {
+
+    let tx = await (await pact.createPact(isEditable_, pactText_, secondsToMaturity_, votingEnabled_, particpantAddresses_, participantsCanVoteArray_, beneficiaryTypes, { value })).wait()
+    let resultingEvent = tx.events && tx.events[0].decode && tx.events[0].decode(tx.events[0].data)
+
+    // console.log("result: ", "\n creator: ", resultingEvent.creator, "\n uid: ", resultingEvent.uid)
+    return {resultingEvent, tx}
 }
 
 const [testCreatePact, testSigning, testPactions, testdispute] = [true, false, true, false]
@@ -36,27 +43,56 @@ describe("WordPact", function () {
     })
 
     if (testCreatePact)
-        describe("Create Pact", function () {
+        describe("Create And edit", function () {
             it("Should allow creating a pact with various inputs", async function () {
-
-                let isEditable_ = false
-                let  pactText_ = "Test pact"
-                let maturityTimeStamp_ = 0
-                let votingEnabled_ = true
-                let particpantAddresses_ = ["0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"]
-                let participantsCanVoteArray_ = [false]
-                let beneficiaryTypes = [0]
-
-                let tx = await (await pact.createPact(isEditable_, pactText_, maturityTimeStamp_, votingEnabled_, particpantAddresses_, participantsCanVoteArray_, beneficiaryTypes, {value: 0})).wait()
-                let resultingEvent = tx.events && tx.events[0].decode && tx.events[0].decode(tx.events[0].data)
-                
-                console.log("result: ", "\n creator: ", resultingEvent.creator, "\n uid: ", resultingEvent.uid )
-
+                let {resultingEvent} = await createNewPact()
                 expect(resultingEvent.creator).to.eq(creator.address)
                 expect(resultingEvent.uid).to.have.length(66)
-
-                let pactCreated = await pact.getPact(resultingEvent.uid)
-                console.log(pactCreated)
+                // let pactCreated = await pact.getPact(resultingEvent.uid)
+                // console.log(pactCreated)
+            })
+            it("should allow editing an editable pact", async function () {
+                let {resultingEvent} = await createNewPact(0, true)
+                let newText = "New text 01"
+                await pact.setText(resultingEvent.uid || "", newText)
+                expect((await pact.getPact(resultingEvent.uid)).pactText).to.eq(newText)
+                await pact.setText(resultingEvent.uid || "", newText+"2")
+                expect((await pact.getPact(resultingEvent.uid)).pactText).to.eq(newText+"2")
+            })
+            it("should not allow editing a non-editable pact", async function() {
+                let {resultingEvent} = await createNewPact(0, false)
+                let newText = "New text 01"
+                await expect(pact.setText(resultingEvent.uid || "", newText)).to.be.reverted
             })
         })
+
+    describe("With Balance", function () {
+        it("should allow adding value to the pact while deploying", async function(){
+            let value = 1000
+            let balanceBefore = await ethers.provider.getBalance(pact.address)
+            let {resultingEvent} = await createNewPact(value)
+            let balanceAfter = await ethers.provider.getBalance(pact.address)
+            let contributionAfter = await pact.contributions(resultingEvent.uid, creator.address)
+            expect(balanceAfter).to.eq(balanceBefore.add(value))
+            expect(contributionAfter).to.eq(value)
+        })
+
+        it("should allow withdrawing when no timelock", async function() {
+            let value = 1000
+            let pactDetails = await createNewPact(value)
+            let balanceBefore = await ethers.provider.getBalance(creator.address)
+            let tx = await (await pact.withdraw(pactDetails.resultingEvent.uid, value)).wait()
+            let balanceAfter = await ethers.provider.getBalance(creator.address)
+            expect(balanceAfter).to.eq(balanceBefore.add(value).sub(tx.gasUsed.mul(tx.effectiveGasPrice)))
+        })
+
+        it("should allow adding a timelock to the balance while deploying", async function(){
+            let maturitySeconds = 1
+            let {resultingEvent} = await createNewPact(10, false, "t", maturitySeconds)
+            expect((await pact.pacts(resultingEvent.uid)).maturityTimeStamp).to.eq(
+                (await ethers.provider.getBlock(await ethers.provider.getBlockNumber())).timestamp
+                + maturitySeconds
+            )
+        })
+    })
 });
