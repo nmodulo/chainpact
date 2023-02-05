85ffe1af9c5ad7b1c2428761c1a4c99eb1f8238d
Somnath B <somnathb@nmodulo.com>
2023-02-05 19:57:10 +0530

    Add mumbai, goerli, bsctest

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -23,6 +23,18 @@ const config: HardhatUserConfig = {
     fuji: {
       url: "https://api.avax-test.network/ext/bc/C/rpc",
       accounts: [process.env.FUJI_PRIVATE_KEY_0 ?? "0x0"]
+    }, 
+    goerli: {
+      url: "https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
+      accounts: [process.env.GOERLI_PRIVATE_KEY_0 ?? "0x0"]
+    },
+    mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.FUJI_PRIVATE_KEY_0 ?? "0x0"]    
+    },
+    bsctest: {
+      url: "https://data-seed-prebsc-2-s1.binance.org:8545",
+      accounts: [process.env.FUJI_PRIVATE_KEY_0 ?? "0X0"]
     }
   }
 };
